92c0977835d63af6dc562b811c7fd060b0b63875
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-11-08 17:28:03 -0500

    haciendo la migracion y base de datos

---
+++ Migrations/20211108222651_Inicial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P2_AP1_Julio_Cesar.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TiposTareas",
+                columns: table => new
+                {
+                    TipoTareaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Acomulado = table.Column<int>(type: "INTEGER", nullable: false),
+                    DecripcionTipoTarea = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposTareas", x => x.TipoTareaId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TiposTareas",
+                columns: new[] { "TipoTareaId", "Acomulado", "DecripcionTipoTarea" },
+                values: new object[] { 1, 0, "Analisis" });
+
+            migrationBuilder.InsertData(
+                table: "TiposTareas",
+                columns: new[] { "TipoTareaId", "Acomulado", "DecripcionTipoTarea" },
+                values: new object[] { 2, 0, "Diseño" });
+
+            migrationBuilder.InsertData(
+                table: "TiposTareas",
+                columns: new[] { "TipoTareaId", "Acomulado", "DecripcionTipoTarea" },
+                values: new object[] { 3, 0, "Programacion" });
+
+            migrationBuilder.InsertData(
+                table: "TiposTareas",
+                columns: new[] { "TipoTareaId", "Acomulado", "DecripcionTipoTarea" },
+                values: new object[] { 4, 0, "Prueba" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TiposTareas");
+        }
+    }
+}
