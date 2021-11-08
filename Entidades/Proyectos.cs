a07a7cbf4b261aef6fd267b97fed71c942c46d97
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-11-08 18:00:25 -0500

    Entidades Lista

---
+++ Entidades/Proyectos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Julio_Cesar.Entidades
+{
+   public class Proyectos
+    {
+        [Key]
+        public int ProyectoId { get; set; }
+        public int Total { get; set; }
+        public string DecripcionProyecto { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [ForeignKey("ProyectoId")]
+        public virtual List<ProyectosDetalle> Detalle { get; set; } = new List<ProyectosDetalle>();
+
+    }
+}
---
+++ Entidades/ProyectosDetalle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Julio_Cesar.Entidades
+{
+   public class ProyectosDetalle
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public int TipoTareaId { get; set; }
+        public int ProyectoId { get; set; }
+        public string Requerimiento { get; set; }
+        public int Tiempo { get; set; }
+    
+        [ForeignKey("TipoTareaId")]
+        public TiposTareas TiposTareas { get; set; }
+        public Proyectos proyecto { get; set; }
+
+        public ProyectosDetalle()
+        {
+            Id = 0;
+            ProyectoId = 0;
+            TipoTareaId = 0;
+            Tiempo = 0;
+            Requerimiento = string.Empty;
+            TiposTareas = null;
+            proyecto = null;
+        }
+       
+
+    }
+
+
+}
